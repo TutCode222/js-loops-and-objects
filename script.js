a888ddb0a74b7612e5dedf8264c426f88faee5f1
TutCode222 <tuddycarraway@gmail.com>
2025-04-17 20:35:14 -0500

    Answered Problems 1 and 2 of Part 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// PART #1
+
+// Problem 1: Count to 10
+// Use a for loop to print numbers 1 through 10 in the console.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+
+
+  // Problem 2: Even Numbers
+// Print all even numbers from 2 to 20 using a for loop.
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+  }
